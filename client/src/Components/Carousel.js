ebfb8d9e04ce7538f03638acb4d5494a23480884
Priyanshu Anand <priyanshuanandd@gmail.com>
2024-10-16 16:32:08 +0530

    Carousel changes

--- client/src/Components/Carousel.js
+++ client/src/Components/Carousel.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Carousel() {
     return (
-        <div className="carousel w-full">
-            <div id="slide1" className="carousel-item relative w-full">
+        <div className="carousel w-full h-50">
+            <div id="slide1" className="carousel-item relative w-full ">
                 <img
                     src="https://picsum.photos/1920/500/"
                     className="w-full" />
@@ -14,7 +14,7 @@ function Carousel() {
             </div>
             <div id="slide2" className="carousel-item relative w-full">
                 <img
-                    src="https://picsum.photos/1920/500/"
+                    src="https://picsum.photos/1920/400/"
                     className="w-full" />
                 <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
                     <a href="#slide1" className="btn btn-circle">❮</a>
@@ -23,7 +23,7 @@ function Carousel() {
             </div>
             <div id="slide3" className="carousel-item relative w-full">
                 <img
-                    src="https://picsum.photos/1920/500/"
+                    src="https://picsum.photos/1920/600/"
                     className="w-full" />
                 <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
                     <a href="#slide2" className="btn btn-circle">❮</a>
